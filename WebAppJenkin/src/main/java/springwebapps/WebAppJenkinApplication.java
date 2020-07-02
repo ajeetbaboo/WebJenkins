ffccedabde2fc09edc46a60072c621d9ae9e96e0
Ajeet kumar <ajeetkumarmrj@gmail.com>
2020-07-02 17:51:47 +0530

    web and jenkin

---
+++ WebAppJenkin/src/main/java/springwebapps/WebAppJenkinApplication.java
@@ -0,0 +1,13 @@
+package springwebapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppJenkinApplication.class, args);
+	}
+
+}
